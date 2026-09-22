d5417ea1cf4fd349e79c5cf9ca58256f0b32607e
morsi13 <m.orsi13@studenti.unipi.it>
2026-09-22 10:00:16 +0200

    first commitment of utils.py

---
+++ utils.py
@@ -0,0 +1,17 @@
+"""
+first file on my repo !!!
+
+it will go away soon
+"""
+
+def square(x):
+    """return the square of x"""
+    return x * x
+
+
+if __name__ == "__main__":
+    print(square(5))
+
+    """ sintassi molto comoda in python per fare il test di un modulo,
+      se lo eseguo come script allora esegue il codice
+        sotto if __name__ == "__main__": """
